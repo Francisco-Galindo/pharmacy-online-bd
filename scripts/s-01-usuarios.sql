395c90f34b933d213552272bf7e80f44441063f6
Francisco-Galindo <yo@franciscogalindo.com>
2024-12-06 09:27:46 -0600

    Agrego script s-01-usuarios.sql

--- scripts/s-01-usuarios.sql
+++ scripts/s-01-usuarios.sql
@@ -1,3 +1,37 @@
 --@Autor(es): Francisco Galindo, Andrea Saldaña
 --@Fecha creación: 03/12/2024
 --@Descripción: Script para la creación de los usuarios utilizados en el proyecto
+
+
+prompt Conectando a la pdb fgmbd_s1
+connect sys/system1@fgmbd_s1 as sysdba
+
+
+prompt Creando roles
+
+drop role if exists rol_admin;
+create role rol_admin;
+grant
+  create session,
+  create table,
+  create view,
+  create synonym,
+  create sequence,
+to rol_admin;
+
+drop role if exists rol_invitado;
+create role rol_admin;
+grant create session to rol_invitado;
+
+
+prompt Creando usuarios
+
+drop user if exists gs_proy_invitado;
+create user gs_proy_invitado identified by gs_proy_invitado quota unlimited on users;
+grant rol_invitado to gs_proy_invitado;
+
+drop user if exists gs_proy_admin;
+create user gs_proy_admin identified by gs_proy_admin quota unlimited on users;
+grant rol_admin to gs_proy_admin;
+
+disconnect
