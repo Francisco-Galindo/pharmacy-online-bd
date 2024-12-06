152d18d339a5271877740c2fdd8e377038492b01
Francisco-Galindo <yo@franciscogalindo.com>
2024-12-06 13:00:50 -0600

    Primera iteración de script s-03-tablas-temporales.sql

--- scripts/s-03-tablas-temporales.sql
+++ scripts/s-03-tablas-temporales.sql
@@ -1,3 +1,12 @@
 --@Autor(es): Francisco Galindo, Andrea Saldaña
 --@Fecha creación: 03/12/2024
 --@Descripción: Script para la creación de tablas temporales
+
+create private temporary table ora$ptt_carrito_compras (
+  presentacion_id         not null,
+  cantidad  number(10,0)  not null
+  constraint carrito_compras_presentacion_id_uk unique(presentacion_id),
+  constraint carrito_compras_presentacion_id_fk
+    foreign key (presentacion_id)
+    references presentacion(presentacion_id)
+) on commit drop definition;
