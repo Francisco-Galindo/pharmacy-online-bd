7ffb0848ba32f3a503858c0d49bd3c408852b817
Francisco-Galindo <yo@franciscogalindo.com>
2024-12-10 00:00:50 -0600

    Trigger para calcular el importe de un pedido

---
+++ scripts/s-11-tr-calcula-importe-pedido.sql
@@ -0,0 +1,45 @@
+--@Autor(es): Francisco Galindo, Andrea Saldaña
+--@Fecha creación: 03/12/2024
+--@Descripción: Script con la definición de un trigger para cumplir con las
+--              reglas de negocio referentes a los status de un pedido.
+
+connect gs_proy_admin/gs_proy_admin@&p_pdb
+
+create or replace trigger tr_calcula_importe_pedido
+  before
+    insert or
+    update of es_valido
+  on medicamento_pedido
+  for each row
+declare
+  v_precio_medicamento  presentacion.precio%type;
+  v_descuento_cliente   cliente.descuento%type;
+begin
+  select precio into v_precio_medicamento
+    from presentacion
+    where presentacion_id = :new.presentacion_id;
+
+  case
+  when inserting then
+    if :new.es_valido = true then
+      update pedido
+        set importe = importe + (v_precio_medicamento * (1 - v_descuento_cliente))
+        where pedido_id = :new.pedido_id;
+    end if;
+  when updating then
+    if :old.es_valido = true and :new.es_valido = false then
+      update pedido
+        set importe = importe - (v_precio_medicamento * (1 - v_descuento_cliente))
+        where pedido_id = :new.pedido_id;
+    elsif :old.es_valido = false and :new.es_valido = true then
+      update pedido
+        set importe = importe + (v_precio_medicamento * (1 - v_descuento_cliente))
+        where pedido_id = :new.pedido_id;
+    end if;
+  end case;
+end;
+/
+
+show errors
+
+disconnect;
