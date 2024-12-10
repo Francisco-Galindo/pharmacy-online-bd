3ef031ab082d36e9846e41f4c04fb34c737fc24e
Francisco-Galindo <yo@franciscogalindo.com>
2024-12-10 13:13:57 -0600

    A

--- scripts/s-00-main.sql
+++ scripts/s-00-main.sql
@@ -7,6 +7,7 @@ define p_root_dir='/unam/bd/pharmacy-online-bd'
 @&p_root_dir/scripts/s-02-entidades.sql
 @&p_root_dir/scripts/s-04-tablas-externas.sql
 @&p_root_dir/scripts/s-06-indices.sql
+@&p_root_dir/scripts/s-07-sinonimos.sql
 @&p_root_dir/scripts/s-09-carga-inicial.sql
 
 @&p_root_dir/scripts/s-15-fx-crear-folio-pedido.sql
@@ -17,7 +18,14 @@ define p_root_dir='/unam/bd/pharmacy-online-bd'
 @&p_root_dir/scripts/s-13-p-actualiza-inventario.sql
 @&p_root_dir/scripts/s-11-tr-checa-estados-pedido.sql
 @&p_root_dir/scripts/s-11-tr-calcula-importe-pedido.sql
+
 @&p_root_dir/scripts/s-13-p-ingresa-operacion.sql
 -- @&p_root_dir/scripts/s-03-tablas-temporales.sql
 @&p_root_dir/scripts/s-13-p-pedir-desde-carrito.sql
 @&p_root_dir/scripts/s-13-p-carga-archivo.sql
+
+@&p_root_dir/scripts/s-12-tr-calcula-importe-pedido-prueba.sql
+@&p_root_dir/scripts/s-12-tr-checa-estados-pedido-prueba.sql
+
+
+commit;
